0ecb1c2a7c9102d6555443ae2e4416204e10ad34
codrata <clintonoye@codrata.com>
2019-02-28 07:42:44 +0100

    Refactored the Notes

---
+++ app/src/main/java/com/example/android/smartnote/Notes/ChmOneTwo.java
@@ -0,0 +1,15 @@
+package com.example.android.smartnote.Notes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.smartnote.R;
+
+public class ChmOneTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chm_one_two);
+    }
+}
---
+++ app/src/main/java/com/example/android/smartnote/Notes/PhyOneOne.java
@@ -0,0 +1,15 @@
+package com.example.android.smartnote.Notes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.smartnote.R;
+
+public class PhyOneOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phy_one_one);
+    }
+}
